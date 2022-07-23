5857e8f63744cdcf1cbb9dda0eae8315d60b2f23
Gedelachandrasekhar126396 <gedelachandrasekhar@gmail.com>
2022-07-23 15:08:34 +0530

    updated

--- jss files/index.js
+++ jss files/index.js
@@ -1,4 +1,4 @@
-// index file js is here
+// index files starts here//
 
 document.querySelector("#menu").addEventListener("click",display)
 
@@ -12,12 +12,12 @@ document.querySelector("#menubar").style="display:none"
 }
 let array = [
         
-        { image : "https://images.firstpost.com/wp-content/uploads/fpranking/1658499549883.jpg", c:1},
-        { image : "https://images.firstpost.com/wp-content/uploads/2021/10/14reliance.jpg?impolicy=website&width=640&height=362", c:2},
-        { image : "https://images.firstpost.com/wp-content/uploads/2021/10/Liquor6432.jpg?impolicy=website&width=640&height=362", c:3},
-        { image : "https://images.firstpost.com/wp-content/uploads/2022/07/640363-2022-07-22T194527.085.jpg?impolicy=website&width=640&height=362", c:4},
-        { image : "https://images.firstpost.com/wp-content/uploads/2022/06/mobile11.jpg?impolicy=website&width=640&height=362", c:5},
-        { image : "https://images.firstpost.com/wp-content/uploads/fpranking/165833894398.jpg", c:0},
+        { image : "https://images.firstpost.com/wp-content/uploads/fpranking/1658499549883.jpg", c:1, story : "Reliance Industries reports 46.3% year-on-year rise in consolidated net profit for quarter ended June"},
+        { image : "https://images.firstpost.com/wp-content/uploads/fpranking/1658562615119.png", c:2,story : "Right Word | Congress’ VP candidate justifies Emergency, says ‘kept mum and fell in line’"},
+        { image : "https://images.firstpost.com/wp-content/uploads/2022/07/Uddhav-and-Eknath1.jpg?impolicy=website&width=640&height=362", c:3,story : "Whose Shiv Sena? Election Commission asks Uddhav Thackeray, Eknath Shinde to prove claims"},
+        { image : "https://images.firstpost.com/wp-content/uploads/2021/03/Partha-Chatterjee-640.jpg?impolicy=website&width=640&height=362", c:4,story : "SSC recruitment scam: ED arrests TMC Minister Partha Chatterjee"},
+        { image : "https://images.firstpost.com/wp-content/uploads/2022/05/rsz_yasin_malik.jpg?impolicy=website&width=640&height=362", c:5,story : "Terrorist Yasin Malik begins hunger strike inside Tihar jail, alleges his case is not being 'investigated properly'"},
+        { image : "https://images.firstpost.com/wp-content/uploads/fpranking/165833894398.jpg", c:0,story : " British PM contest: Rishi Sunak races ahead with 137 votes, to face Liz Truss in final leg"},
 ]
 let count=0;
 console.log(count)   
@@ -28,7 +28,8 @@ console.log(count)
         console.log(count)
        for(let i=0;i<array.length;i++){
         if (base=== array[i].c){
-            document.querySelector("#pic").src = array[i].image
+            document.querySelector("#pic").src = array[i].image;
+            document.querySelector(".big-story").innerText = array[i].story;
         }
        }
      }
@@ -40,7 +41,8 @@ console.log(count)
         console.log(count)
        for(let i=0;i<array.length;i++){
         if (base=== array[i].c){
-            document.querySelector("#pic").src = array[i].image
+            document.querySelector("#pic").src = array[i].image;
+            document.querySelector(".big-story").innerText = array[i].story;
         }
        }
      }
